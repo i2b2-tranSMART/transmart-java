fa1d8f459864b1a4a0d0d39f0ad94dbe69d8d002
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 12:46:01 -0500

    cleanup, version updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,38 +1,26 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
-grails.project.dependency.resolver = "maven"
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false
-    // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
-    repositories {
-        grailsCentral()
-        mavenCentral()
 
-        mavenRepo "https://repo.transmartfoundation.org/content/repositories/public/"
-        mavenRepo "https://repo.thehyve.nl/content/repositories/public/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	inherits 'global'
+	log 'warn'
+	legacyResolve false
 
-        // runtime 'mysql:mysql-connector-java:5.1.21'
-        runtime 'com.oracle:ojdbc7:12.1.0.1', {
-            export = false
-        }
+	repositories {
+		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
+		grailsCentral()
+		mavenCentral()
+		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
+	}
 
-    }
+	dependencies {
+		runtime 'com.oracle:ojdbc7:12.1.0.1', { export = false }
+	}
 
-    plugins {
-        build(":release:3.0.1",
-                ":rest-client-builder:1.0.3") {
-            export = false
-        }
-    }
+	plugins {
+		build ':release:3.1.0', ':rest-client-builder:2.1.0', {
+			export = false
+		}
+	}
 }
