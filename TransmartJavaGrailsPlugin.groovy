1187d8c047c0b528819b2008b875a65f785e7d29
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 13:58:54 -0500

    bumped version to 18.1-SNAPSHOT

--- TransmartJavaGrailsPlugin.groovy
+++ TransmartJavaGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class TransmartJavaGrailsPlugin {
-	def version = '16.2'
+	def version = '18.1-SNAPSHOT'
 	def grailsVersion = '2.3 > *'
 	def title = 'tranSMART Java Backend'
 	def author = 'David Newton'
