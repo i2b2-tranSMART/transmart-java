51a497e864b55d2dd2e969299fe667afc18bd252
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 13:56:54 -0500

    removed unnecessary ojdbc7 dependency, and deleted all jar files in the lib directory except GenePattern.jar and added BuildConfig dependencies instead; also deleted from GenePattern.jar the org.json.* classes and the annoying log4j.properties file that creates empty System.out files everywhere

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,22 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		runtime 'com.oracle:ojdbc7:12.1.0.1', { export = false }
+		compile 'axis:axis:1.4', {
+			excludes 'axis-saaj', 'commons-logging'
+		}
+		compile 'com.jcraft:jsch:0.1.54'
+		compile 'com.thoughtworks.xstream:xstream:1.3', {
+			excludes 'xpp3_min'
+		}
+		compile 'commons-net:commons-net:3.3'
+		compile 'jfree:jfreechart:1.0.11', {
+			excludes 'gnujaxp', 'jcommon', 'junit'
+		}
+		compile 'net.sf.opencsv:opencsv:2.3'
+		compile 'org.apache.lucene:lucene-core:2.4.0'
+		compile 'org.apache.lucene:lucene-highlighter:2.4.0'
+		compile 'org.apache.poi:poi:3.1-FINAL'
+		compile 'org.json:json:20090211'
 	}
 
 	plugins {
